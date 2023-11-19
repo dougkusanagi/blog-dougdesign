c5435e5786f44ff0ccd72abb6e64cc35ec31e369
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-11-19 14:21:49 -0300

    install filament

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+// @ts-check
+const { fontFamily } = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -5,8 +9,64 @@ export default {
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
+    // darkMode: "class",
     theme: {
-        extend: {},
+        extend: {
+            lineHeight: {
+                11: "2.75rem",
+                12: "3rem",
+                13: "3.25rem",
+                14: "3.5rem",
+            },
+            fontFamily: {
+                // sans: ["var(--font-space-grotesk)", ...fontFamily.sans],
+                sans: "Grotesk, sans-serif",
+            },
+            colors: {
+                primary: colors.pink,
+                gray: colors.gray,
+            },
+            typography: ({ theme }) => ({
+                DEFAULT: {
+                    css: {
+                        a: {
+                            color: theme("colors.primary.500"),
+                            "&:hover": {
+                                color: `${theme("colors.primary.600")}`,
+                            },
+                            code: { color: theme("colors.primary.400") },
+                        },
+                        "h1,h2": {
+                            fontWeight: "700",
+                            letterSpacing: theme("letterSpacing.tight"),
+                        },
+                        h3: {
+                            fontWeight: "600",
+                        },
+                        code: {
+                            color: theme("colors.indigo.500"),
+                        },
+                    },
+                },
+                invert: {
+                    css: {
+                        a: {
+                            color: theme("colors.primary.500"),
+                            "&:hover": {
+                                color: `${theme("colors.primary.400")}`,
+                            },
+                            code: { color: theme("colors.primary.400") },
+                        },
+                        "h1,h2,h3,h4,h5,h6": {
+                            color: theme("colors.gray.100"),
+                        },
+                    },
+                },
+            }),
+        },
     },
-    plugins: [],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+    ],
 };
