432556a924ee91e3093f5a33f884b39189f1e34c
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-11-19 18:34:43 -0300

    make post mfs

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->sentence();
+
+        return [
+            'title' => $title,
+            'slug' => str($title)->slug(),
+            'body' => fake()->realText(1000),
+            'excerpt' => fake()->sentence(),
+            'active' => rand(0, 1),
+        ];
+    }
+}
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $posts = Post::factory(50)
+            ->create();
+
+        $posts->each(function (Post $post) {
+            $post->categories()->attach(
+                Category::inRandomOrder()
+                    ->limit(2)
+                    ->get()
+            );
+        });
+    }
+}
