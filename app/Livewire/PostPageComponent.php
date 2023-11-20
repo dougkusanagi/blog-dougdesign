929dffc6370deb97d7e98988acf537f77f4816a5
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-11-20 11:23:49 -0300

    fix get post by slug in post.blade

--- app/Livewire/PostPageComponent.php
+++ app/Livewire/PostPageComponent.php
@@ -2,16 +2,25 @@
 
 namespace App\Livewire;
 
-use App\Models\Post as ModelsPost;
+use App\Models\Post;
 use Livewire\Component;
 
 class PostPageComponent extends Component
 {
     public $post;
 
-    public function mount(ModelsPost $post)
+    public function mount(string $slug)
     {
-        $this->post = ModelsPost::first();
+        $this->post = Post::query()
+            ->where('slug', $slug)
+            ->where('published', true)
+            ->where('deleted_at', null)
+            ->with(['categories'])
+            ->first();
+
+        if (!$this->post) {
+            abort(404);
+        }
     }
 
     public function render()
