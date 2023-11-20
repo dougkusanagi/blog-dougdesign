c3d5f568f97e1babebb778cd24dbdcaab7da64ff
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-11-20 16:53:45 -0300

    add some meta infos for SEO

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,25 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description"
+        content="{{ $description ?? 'Junte-se a centenas de leitores e acelere seu aprendizado de programação.' }}" />
 
-    <title>{{ $title ?? 'Page Title' }}</title>
+    <meta property="og:title" content="{{ $title ?? config('app.name') }}" />
+    {{-- <meta property="og:image"
+        content="{{ $image ?? 'https://i.useflipp.com/gw6mxpkgy4v8.png?title=' . urlencode($title ?? '') . '&body=' . urlencode($description ?? '') . '&watermark=useflipp.com' }}" /> --}}
+    <meta property="og:image" content="/images/og-image.png" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ url()->current() }}" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:creator" content="@douglopesreal" />
+    <meta name="twitter:description"
+        content="{{ $description ?? 'Junte-se a centenas de leitores e acelere seu aprendizado de programação.' }}" />
+    <meta name="twitter:image" content="{{ $image ?? '/images/og-image.png' }}" />
+    <meta name="twitter:title" content="{{ $title ?? config('app.name') }}" />
+    <meta name="theme-color" content="#ec4899" />
+
+    <title>{{ $title ?? 'Blog Doug Design' }}</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -15,9 +32,9 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="antialiased text-black bg-white dark:bg-gray-950 dark:text-white">
-    <section class="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
-        <div class="flex flex-col justify-between h-screen font-sans">
+<body class="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
+    <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+        <div class="flex h-screen flex-col justify-between font-sans">
             <x-header />
 
             <main class="mb-auto">
