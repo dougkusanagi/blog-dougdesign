351b43b9909f3ce71ffb551396ab50fcfd06146b
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-11-22 16:00:55 -0300

    move gtag

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,19 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="scroll-behavior: smooth">
 
 <head>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6VNXK1N5QD"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-6VNXK1N5QD');
-    </script>
-
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description"
@@ -46,6 +33,19 @@
 
     @filamentStyles
     @vite('resources/css/app.css')
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6VNXK1N5QD"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-6VNXK1N5QD');
+    </script>
 </head>
 
 <body class="antialiased text-black bg-white dark:bg-gray-950 dark:text-white">
