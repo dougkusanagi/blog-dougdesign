f506c6daba7e37fa98ff98240c8b04ef750b8484
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-11-22 20:49:33 -0300

    add artisan sitemap:generate

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\GenerateSitemapService;
+use Illuminate\Console\Command;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    // protected $signature = 'app:generate-sitemap';
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Gera o sitemap da aplicação';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $generate_sitemap_service = new GenerateSitemapService();
+        $generate_sitemap_service->handle();
+    }
+}
---
+++ app/Services/GenerateSitemapService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Post;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
+
+class GenerateSitemapService
+{
+    public function handle()
+    {
+        $path = public_path('sitemap.xml');
+        $url = config('app.url', 'http://blog.dougdesign.com.br');
+        $sitemap = Sitemap::create();
+
+        Post::all()->each(function ($post) use ($url, $sitemap) {
+            // $sitemap->add($url . '/' . $post->slug);
+
+            $sitemap->add(
+                Url::create($url . '/' . $post->slug)
+                    ->setPriority(0.9)
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+            );
+        });
+
+        $sitemap->writeToFile($path);
+    }
+}
