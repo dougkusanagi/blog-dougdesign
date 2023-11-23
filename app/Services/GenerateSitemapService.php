2f2988c1037b087f153b761d2098ed254d8e042d
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-11-23 00:11:58 -0300

    small clean up

--- app/Services/GenerateSitemapService.php
+++ app/Services/GenerateSitemapService.php
@@ -4,20 +4,17 @@ namespace App\Services;
 
 use App\Models\Post;
 use Spatie\Sitemap\Sitemap;
-use Spatie\Sitemap\SitemapGenerator;
 use Spatie\Sitemap\Tags\Url;
 
 class GenerateSitemapService
 {
     public static function handle()
     {
-        $path = public_path('sitemap.xml');
         $url = config('app.url', 'http://blog.dougdesign.com.br');
         $sitemap = Sitemap::create();
+        $path = public_path('sitemap.xml');
 
         Post::all()->each(function ($post) use ($url, $sitemap) {
-            // $sitemap->add($url . '/' . $post->slug);
-
             $sitemap->add(
                 Url::create($url . '/' . $post->slug)
                     ->setPriority(0.9)
