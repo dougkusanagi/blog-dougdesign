287d915ef3b99589bc61d5c419ee8dd8c9abc578
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-11-29 15:00:09 -0300

    favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -27,7 +27,7 @@
     <title>{{ $title ?? 'Blog Doug Design' }}</title>
 
     {{-- favicon --}}
-    <link rel="icon" href="/favicon.ico" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
