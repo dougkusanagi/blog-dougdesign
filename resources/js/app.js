c50548409656d9d616cd2affe7d72db0074370a8
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-12-02 00:38:53 -0300

    fixed nprogress twice

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,1 @@
-import nProgress from "nprogress";
-import {
-    Alpine,
-    Livewire,
-} from "../../vendor/livewire/livewire/dist/livewire.esm";
 import "./bootstrap";
-
-Livewire.hook("commit", ({ component, commit, respond, succeed, fail }) => {
-    nProgress.start();
-
-    succeed(({ snapshot, effect }) => {
-        queueMicrotask(() => {
-            nProgress.done(true);
-            nProgress.remove();
-        });
-    });
-});
