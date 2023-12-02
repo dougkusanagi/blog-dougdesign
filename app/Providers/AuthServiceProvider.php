6535333fa896d9487e7ad8a9a698b2520302c152
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-12-02 00:34:58 -0300

    define pulse gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,8 +22,8 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      */
-    public function boot(): void
+    public function boot(User $user): void
     {
-        //
+        Gate::define('viewPulse', fn () => $user !== null);
     }
 }
